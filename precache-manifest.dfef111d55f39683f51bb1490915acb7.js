1edfc58fcb05ec79d2c2465a01b1968d4a1fa285
Eugene <evgnkuchma@gmail.com>
2020-04-29 06:52:33 +0300

    Updates

---
+++ precache-manifest.dfef111d55f39683f51bb1490915acb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4cfa2e2fe7ebd268ce0b12775aee00d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b88fccc24c878b502049",
+    "url": "./static/css/2.cd1899df.chunk.css"
+  },
+  {
+    "revision": "f8bbea61ba89ead759ae",
+    "url": "./static/css/main.9f604ba9.chunk.css"
+  },
+  {
+    "revision": "b88fccc24c878b502049",
+    "url": "./static/js/2.749da832.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.749da832.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8bbea61ba89ead759ae",
+    "url": "./static/js/main.31363cc7.chunk.js"
+  },
+  {
+    "revision": "1957fe11dd2dd30cfae0",
+    "url": "./static/js/runtime-main.701f4094.js"
+  }
+]);
